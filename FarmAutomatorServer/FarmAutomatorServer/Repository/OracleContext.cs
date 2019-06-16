1df80e192fe23a526b2a0febf6cc59fd14150f5e
Vuong Nguyen <vuong.se@gmail.com>
2019-06-17 06:54:15 +0700

    .

---
+++ FarmAutomatorServer/FarmAutomatorServer/Repository/OracleContext.cs
@@ -0,0 +1,19 @@
+﻿using FarmAutomatorServer.Repository.DbModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FarmAutomatorServer.Repository
+{
+    public class OracleContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseOracle(@"User Id=Scott;Password=tiger;Data Source=Ora;");
+        }
+    }
+}
---
+++ Oracle EF Code First Example.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Migrations;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Migrations.History;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<OracleDbContext>());
+
+            using (var ctx = new OracleDbContext())
+            {
+                var emp = new Employee
+                {
+                    Name = "Tom",
+                    HireDate = DateTime.Now
+                };
+
+                ctx.Employees.Add(emp);
+                ctx.SaveChanges();
+
+                var dept = new Department
+                {
+                    Name = "Accounting",
+                    ManagerId = emp.EmployeeId
+                };
+
+                ctx.Departments.Add(dept);
+                ctx.SaveChanges();
+            }
+
+            Console.Write("Press any key to continue... ");
+            Console.ReadLine();
+        }
+    }
+
+
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public DateTime HireDate { get; set; }
+        //public string Location { get; set; }
+    }
+
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        [ForeignKey("Manager")]
+        public int ManagerId { get; set; }
+        public Employee Manager { get; set; }
+    }
+
+    public class OracleDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("HR");
+        }
+    }
+}
